086a452d7e91db0e7f78a006d7c04e792268a6e6
technicalGabs <160118100+technicalGabs@users.noreply.github.com>
2025-04-07 20:37:54 +0200

    Testing Jest

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+// Testing Jest
+function add(addEventListener,b){
+    return a+b;
+}
+
+
+
+//------------------ METHODS BEING TESTED---------------------
+module.exports = { add}
+
+//------------------------------------------------------------
