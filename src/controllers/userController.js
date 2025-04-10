26266d5f32297ccc447afbdee68ffd24f35a515d
pythonreus <kgadiselepe11@gmail.com>
2025-04-11 01:10:20 +0200

    FLIO-30 , I created our database(mongodb), i created the connection to the database, i separated our backend using the MVC approach, created the main entry point to our backend code which is the main.js, i created the user model for our database, i separated our frontend code to the client folder

---
+++ src/controllers/userController.js
@@ -0,0 +1,31 @@
+const User = require('../models/userModel');
+
+// function to signup
+const userRegister =  (req, res) => {
+        // here i am just testing if all is working properly
+
+        // process the user data
+        const user = new User({
+            id_from_third_party : "234786rt7347ry34yr8u8u",
+            third_party_name : "Google",
+            role : "admin"
+        });
+    
+        user.save()
+          .then((result) =>{
+            // Respond with the received data
+            return res.json({ message: "User Signup was successful!", result: result, error : "none" });
+    
+          })
+          .catch((err) =>{
+            return res.json({ message: "User Signup was unsuccessful!", result: {}, error : err});
+          });
+
+  };
+
+// function to login
+const userlogin = (req,res) => {
+
+};
+
+module.exports =  { userRegister, userlogin };
